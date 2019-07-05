ad88fc6afa21aa8a194917be02bb850c58c9f261
Michal Jarnot <m.jarnot@yahoo.com>
2019-07-05 13:58:12 -0700

    ci: add pre-commit hooks
    - install husky
    - install lint-staged
    - install commit lint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import { logger } from 'src/services/log'
 import { initializeCreateReactApp, installNpx } from 'src/steps'
 
 const main = async () => {
-  logger.info(`This app generator is built on top of Create React App (${chalk.blue('https://github.com/facebook/create-react-app')}.`)
+  logger.info(
+    `This app generator is built on top of Create React App (${chalk.blue(
+      'https://github.com/facebook/create-react-app'
+    )}.`
+  )
 
   await installNpx()
   await initializeCreateReactApp()
