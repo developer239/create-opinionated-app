e1c2eff518398f9db06f16d486e5cfa533377524
Michal Jarnot <m.jarnot@yahoo.com>
2020-01-09 21:59:49 +0100

    fix: remove some-app from git

--- some-app/commitlint.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { extends: ['@code-quality/commitlint-config'] }
--- some-app/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from 'react'
-import { Layout } from 'components/Layout'
-
-const App: FC = () => <Layout>Hello Some App</Layout>
-
-export { App }
