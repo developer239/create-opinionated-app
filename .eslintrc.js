02a4f1191da4ce69ff47cd41e0aa3a9a6c0ef980
Michal Jarnot <m.jarnot@yahoo.com>
2020-01-11 23:15:28 +0100

    feat: update code quality tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,4 @@ module.exports = {
     'prettier',
     'prettier/react'
   ],
-  rules: {
-    'security/detect-non-literal-fs-filename': 1,
-    'default-case': 1,
-    'import/order': ['error', { 'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'] }],
-    'max-lines-per-function': ['error', 90],
-    '@typescript-eslint/triple-slash-reference': 0
-  }
 }
